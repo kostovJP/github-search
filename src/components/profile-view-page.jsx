2d8e7fe90743e6d34b873276ffde0052494415a9
Kaustav <kaustavphukan44@gmail.com>
2025-08-12 20:43:19 +0530

    implemented the search-form

---
+++ src/components/profile-view-page.jsx
@@ -0,0 +1,11 @@
+import SearchForm from "./search-form";
+import { useState, useEffect } from "react";
+
+export default function ProfileViewPage(){
+    
+    return (
+        <main>
+            <SearchForm/>
+        </main>
+    )
+}
--- src/components/search-form.jsx
+++ src/components/search-form.jsx
@@ -1,5 +1,6 @@
 import { useForm } from 'react-hook-form'
 import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button';
 
 export default function SearchForm({ dataFunc }){
     const {
@@ -8,18 +9,29 @@ export default function SearchForm({ dataFunc }){
         formState: {errors, isSubmitted, isSubmitting},
     } = useForm();
 
+    const formSubmission = (evt) => {
+        dataFunc(evt.username);
+        console.log(evt.username);
+    }
     return (
         <form action="/" 
-            className={`border-2 p-2 w-75 md:w-100 justify-self-center m-2
-                    grid grid-cols-2
+            className={`border-none p-2 w-75 md:w-100 justify-self-center m-2
+                    grid grid-cols-3 gap-2 bg-gray-200 rounded-l shadow-sm shadow-neutral-500
                 `}
+            onSubmit={handleSubmit(formSubmission)}
         >
             <TextField id="standard-basic" label="github-username" variant="standard" 
                 {...register("username", {
-                    required: {value: true, message: "username cannot be empty"}
-                    
+                    required: {value: true, message: "*username cannot be empty"},
+                    maxLength: {value: 15, message: "*useranme cannot exceed 15 characters"},
+                    minLength: {value: 2, message: "*username must be atleast 2 characters long"},
                 })}
+                className='col-span-2'
             />
+            <span className='flex justify-center'><Button variant="contained" type="submit">search</Button></span>
+            {errors.username && 
+                <p className='text-[12px] text-red-500 col-span-3'>{errors.username.message}</p>
+            }
         </form>
     )
 }
